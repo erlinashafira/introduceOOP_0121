2ade32f093372e988a58b7198aa8fedef28ca593
errerew <erlina31acc@gmail.com>
2025-04-29 18:35:27 +0700

    mendeklarasikan variable

--- OOP.cpp
+++ OOP.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 class mahasiswa{
+    public:
+    int nim;
+    string nama;
+    float nilai;
+
+void printData(){
+    cout << "NIM :" << nim << endl;
+    cout << "Nama :" << nama << endl;
+    cout << "Nilai :" << nilai << endl;
+}
 
 };
